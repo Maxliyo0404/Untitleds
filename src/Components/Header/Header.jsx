6432f0e43a4d97177c3ae35a0c25812f29333abf
Mahliyo <gasandigital13@gmail.com>
2026-05-11 22:10:39 +0500

    image

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import"./Header.css";
 import React from 'react'
+import logo1 from "./src/image/logo1 (2).svg"
 
-
+ 
 function Header() {
   return (
    <>
    <div className="Header">
     <div className="container">
         <div className="header-container">
-            <a className="logo" href="#"><img src="" alt="" />Dizenfeksiya</a>
+            <a className="logo" href="#"><img src={logo1} alt="" />Dizenfeksiya</a>
             <ul className="header-list">
                 <li><a className="header-link" href="#">Service</a></li>
                 <li><a className="header-link" href="#">Service</a></li>
