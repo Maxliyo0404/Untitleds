b7a37b4e303da9e0e5242bdf70124b9f9fbcab1e
Mahliyo <gasandigital13@gmail.com>
2026-05-11 17:11:58 +0500

    Header logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,20 @@
 import"./Header.css";
 import React from 'react'
 
+
 function Header() {
   return (
    <>
    <div className="Header">
     <div className="container">
         <div className="header-container">
-            <a className="logo" href="#"></a>
+            <a className="logo" href="#"><img src="" alt="" /></a>
+            <ul className="header-list">
+                <li><a className="header-link" href="#"></a></li>
+                <li><a className="header-link" href="#"></a></li>
+                <li><a className="header-link" href="#"></a></li>
+                <li><a className="header-link" href="#"></a></li>
+            </ul>
         </div>
     </div>
    </div>
