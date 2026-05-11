f6db089a00e03f7dce7a75f425db9571f5ce3311
Mahliyo <gasandigital13@gmail.com>
2026-05-11 22:49:56 +0500

    language

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './i18n';
-g
+import './i18/i18n';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
