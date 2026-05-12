6131530e45ba19b9508576a29efdb018779a7285
Mahliyo <gasandigital13@gmail.com>
2026-05-12 15:26:57 +0500

    main-wrapper css

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import React from 'react'
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
+import SectionOne from "./Components/main/SectionOne/SectionOne";
 
 function App() {
   return (
     <>
     <Header/>
     <Hero/>
+    <SectionOne/>
     </>
   )
 }
---
+++ src/Components/main/SectionOne/SectionOne.jsx
@@ -0,0 +1,10 @@
+import "/.SectionOne.css";
+import React from 'react'
+
+function SectionOne() {
+  return (
+    <div>SectionOne</div>
+  )
+}
+
+export default SectionOne
