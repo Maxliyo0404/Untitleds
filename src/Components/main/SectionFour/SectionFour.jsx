3eea272ea6cbf564b9322eb3938ef6e8ca53712e
Mahliyo <gasandigital13@gmail.com>
2026-05-12 23:19:09 +0500

    sectionFour start

--- src/Components/main/SectionFour/SectionFour.jsx
+++ src/Components/main/SectionFour/SectionFour.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 
 function SectionFour() {
   return (
-     
+      <div className="sectionFour">
+        <div className="container">
+            <div className="sectionFour-wrapper">
+                
+            </div>
+        </div>
+      </div>
   )
 }
 
