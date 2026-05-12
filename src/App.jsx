7b5260245e184f1699c4c3f63f16f988061eb437
Mahliyo <gasandigital13@gmail.com>
2026-05-12 16:42:18 +0500

    sectionOne css list fnish

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
 import SectionOne from "./Components/main/SectionOne/SectionOne";
+import SectionTwo from "./Components/main/SectionTwo/SectionTwo";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Header/>
     <Hero/>
     <SectionOne/>
+    <SectionTwo/>
     </>
   )
 }
---
+++ src/Components/main/SectionTwo/SectionTwo.jsx
@@ -0,0 +1,16 @@
+import "./SectionTwo.css";
+import React from 'react'
+
+function SectionTwo() {
+  return (
+    <div className="sectionTwo">
+        <div className="container">
+            <div className="sectionTwo-wrapper">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SectionTwo
