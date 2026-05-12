aa9b7a5cbd1886b43499169e48a5107b1c3f9fa7
Mahliyo <gasandigital13@gmail.com>
2026-05-12 16:49:57 +0500

    sectionTwo start

--- src/Components/main/SectionTwo/SectionTwo.jsx
+++ src/Components/main/SectionTwo/SectionTwo.jsx
@@ -1,12 +1,17 @@
 import "./SectionTwo.css";
 import React from 'react'
+import two_1 from "./image/two_1.svg";
+import { useTranslation } from "react-i18next";
 
 function SectionTwo() {
+     const {t, i18n} = useTranslation();
   return (
     <div className="sectionTwo">
         <div className="container">
             <div className="sectionTwo-wrapper">
+               <div className="two-right">
                 
+               </div>
             </div>
         </div>
     </div>
