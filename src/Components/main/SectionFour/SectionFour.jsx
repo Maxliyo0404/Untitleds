c15383ecbfad7da370a41aeab13ad9b90f4b0f50
Mahliyo <gasandigital13@gmail.com>
2026-05-12 23:21:54 +0500

    sectionFour start

--- src/Components/main/SectionFour/SectionFour.jsx
+++ src/Components/main/SectionFour/SectionFour.jsx
@@ -1,12 +1,16 @@
 import "./SectionFour.css";
 import React from 'react'
+import four_1 from "./image/four_1.svg";
+
 
 function SectionFour() {
   return (
       <div className="sectionFour">
         <div className="container">
             <div className="sectionFour-wrapper">
-                
+                <div className="four-left">
+                    <img src={four_1} alt="rasm" />
+                </div>
             </div>
         </div>
       </div>
