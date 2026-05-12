e6fc96f13d9073b6f2c617ff48343b7c838c98cd
Mahliyo <gasandigital13@gmail.com>
2026-05-12 15:49:53 +0500

    sectionOne

--- src/Components/main/SectionOne/SectionOne.jsx
+++ src/Components/main/SectionOne/SectionOne.jsx
@@ -1,12 +1,33 @@
 import "/.SectionOne.css";
 import React from 'react'
+import { useTranslation } from "react-i18next";
+import { FaBeer, FaAddressBook } from 'react-icons/fa'; 
 
 function SectionOne() {
+     const {t, i18n} = useTranslation();
   return (
     <div className="sectionOne">
         <div className="container">
             <div className="secttionOne_qrapper">
-                
+                <h2 className="one-title">{t ("sectionOne.one-title")}</h2>
+                <hr />
+                <ul className="one-list">
+                    <li className="one-item">
+                     <div className="icon"></div>
+                     <h3 className="list -title">{t ("sectionOne.list-title")}</h3>
+                     <p className="list-text">{t ("sectionone.list-text")}</p>
+                    </li>
+                    <li className="one-item">
+                     <div className="icon"></div>
+                     <h3 className="list -title">{t ("sectionOne.list-title1")}</h3>
+                     <p className="list-text">{t ("sectionone.list-text")}</p>
+                    </li>
+                    <li className="one-item">
+                     <div className="icon"></div>
+                     <h3 className="list -title">{t ("sectionOne.list-title2")}</h3>
+                     <p className="list-text">{t ("sectionone.list-text")}</p>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
