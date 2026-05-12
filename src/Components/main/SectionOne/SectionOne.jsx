171afa0584fe910431d71f79bf28c757ac18d9b7
Mahliyo <gasandigital13@gmail.com>
2026-05-12 15:28:45 +0500

    sectionOne

--- src/Components/main/SectionOne/SectionOne.jsx
+++ src/Components/main/SectionOne/SectionOne.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 
 function SectionOne() {
   return (
-    <div>SectionOne</div>
+    <div className="sectionOne">
+        <div className="container">
+            <div className="secttionOne_qrapper">
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
