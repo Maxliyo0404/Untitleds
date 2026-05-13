1b5aacb666ef5c6020691ea3bd660a636409a831
Mahliyo <gasandigital13@gmail.com>
2026-05-13 07:16:39 +0500

    sectionFive css

--- src/Components/main/SectionFive/SectionFive.jsx
+++ src/Components/main/SectionFive/SectionFive.jsx
@@ -5,33 +5,43 @@ import { FaBeer, FaAddressBook } from 'react-icons/fa';
 
 function SectionFive() {
     const {t, i18n} = useTranslation();
-    const [val, setVal] = useState("basic");
 
   return (
     <div className="sectionFive">
         <div className="container">
             <div className="sectionFive-wrapper">
+                <hr />
+                <div className="five-card">
             <div className="five-left">
                 <h2 className="five-title">{t("sectionFive.five-title")}</h2>
             </div>
              <div className="five-right">
-               <label htmlFor="course-find">{t('sectionFive.select-label')}</label>
-                        <select 
-                            id="course-find" 
-                            className="custom-select"
-                            value={val}
-                            onChange={(e) => setVal(e.target.value)}
-                        >
-                            <option value="basic">{t('sectionFive.options.basic')}</option>
-                            <option value="middle">{t('sectionFive.options.middle')}</option>
-                            <option value="high">{t('sectionFive.options.high')}</option>
-                        </select>
-                        
-                     
-                        <div className="selection-result">
-                             <p>{t(`sectionFive.details.${val}`)}</p>
-                        </div>
+                <div className="select-wrapper">
+                <select className="select">
+                    <option value="select-label">{t("sectionFive.select-label")}</option>
+                    <option value="high ">{t("sectionFive.high")}</option>
+                    <option value="basic ">{t("sectionFive.basic")}</option>
+                    <option value="middle">{t("sectionFive.middle")}</option>
+                </select>
+                </div>
+                <div className="select-wrapper">
+                <select className="select">
+                    <option value="select-label">{t("sectionFive.select-label")}</option>
+                    <option value="high ">{t("sectionFive.high")}</option>
+                    <option value="basic ">{t("sectionFive.basic")}</option>
+                    <option value="middle">{t("sectionFive.middle")}</option>
+                </select>
+                </div>
+                <div className="select-wrapper">
+                <select className="select">
+                    <option value="select-label">{t("sectionFive.select-label")}</option>
+                    <option value="high ">{t("sectionFive.high")}</option>
+                    <option value="basic ">{t("sectionFive.basic")}</option>
+                    <option value="middle">{t("sectionFive.middle")}</option>
+                </select>
+</div>
                     </div>
+            </div>
             </div>
         </div>
     </div>
