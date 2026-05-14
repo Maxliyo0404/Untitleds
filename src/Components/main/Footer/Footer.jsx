c8d9fddea8856b12efe393cdbc81757ec3a66c70
Mahliyo <gasandigital13@gmail.com>
2026-05-14 06:51:01 +0500

    footer start container

---
+++ src/Components/main/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import { useTranslation } from "react-i18next";
+import "./Footer.css";
+import React from 'react'
+
+function Footer() {
+    const {t, i18n} = useTranslation();
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
