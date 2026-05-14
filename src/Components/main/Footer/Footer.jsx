1df7b8e3eaedc2ed1397076e5195954f63a98e7d
Mahliyo <gasandigital13@gmail.com>
2026-05-14 07:20:01 +0500

    footer start wrapper

--- src/Components/main/Footer/Footer.jsx
+++ src/Components/main/Footer/Footer.jsx
@@ -2,6 +2,9 @@ import { useTranslation } from "react-i18next";
 import "./Footer.css";
 import React from 'react'
 import footer_1 from "./image/footer_1.svg";
+import { FaBeer, FaAddressBook } from 'react-icons/fa'; 
+import { CiLocationOn } from "react-icons/ci";
+import { MdOutlineLocalPhone } from "react-icons/md";
 
 function Footer() {
     const {t, i18n} = useTranslation();
@@ -12,7 +15,12 @@ function Footer() {
                 <a className="footer-img" href="#"> <img src={footer_1} alt="rasm" /></a>
                 <ul className=" footer-list">
                     <li className="footer-item">
-                        
+                     <a className="footers" href="#">
+                        <span className="spans"><CiLocationOn /></span>
+                     </a>
+                     <a className="footers" href="#">
+                        <span className="spans"><MdOutlineLocalPhone /></span>
+                     </a>
                     </li>
                 </ul>
             </div>
